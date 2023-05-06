c85d269f03ffe73f015962bb94dd2afc0f759089
DEEPIKATANAKANTI <113338954+DEEPIKATANAKANTI@users.noreply.github.com>
2023-05-06 10:07:05 +0530

    Create MINMAX.R

---
+++ MINMAX.R
@@ -0,0 +1,14 @@
+data<-mtcars
+data
+a<-c(data$mpg)
+new<-mean(data$mpg)
+new
+min1<-min(data$mpg)
+max1<-max(data$mpg)
+minmax<-(a-min1)/(max1-min1)
+minmax
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+P
